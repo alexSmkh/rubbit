698b560403b5dd0e30892843c4a1d22b62e6ca31
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-01-25 14:10:52 +0300

    Update community selector in navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :find_communities
 
   private
+
+  def find_communities
+    @communities = Community.all.order(:title)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
