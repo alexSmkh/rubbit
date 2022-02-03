21894da15202c937a4516433f6d5ac1d08a2e339
Alexander Samokhvalov <alexshikari9333@gmail.com>
2022-02-03 14:53:07 +0300

    Fixed the submission validatons

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -8,6 +8,24 @@ class Submission < ApplicationRecord
   belongs_to :community
 
   validates :title, presence: true
-  validates :body, presence: true
   validates :url, url: true, allow_blank: true
+  validates :body, length: { maximum: 5000 }
+
+  validate :image_or_video
+
+  # validate :text_or_url
+
+  private
+
+  def image_or_video
+    return if submission_image.blank? || submission_video.blank?
+
+    errors.add(:base, 'Add an image or video, not both')
+  end
+
+  def text_or_url
+    return if url.blank? || body.blank?
+
+    errors.add(:base, 'Add a valid URL or text content')
+  end
 end
